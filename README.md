a6bfc40c51abd288a83f73cea817e9f776cc8aba
ksatunin <konstantin.satunin@intel.com>
2019-09-10 18:48:53 +0300

    Added deployment automation

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 This hello world project provides demonstrates how to run C++ applications capable to be run in iExec environment
 Currently it's possible to run only python processes, that is why your app should be converted into form of shared library
 and called from python via ctypes. All fork, spawn, exec methods do not work in iexechub/python-scone image from 8/5/2019
+
+# Publishing application for testing on kovan chain
+1. Edit deploy.py and provide your password to avoid entering it each time on line 27 add to the end: 'default="yourpassword",'
+2. Either start in fresh folder, or remove existing iexec json files, you can avoid it and use script with --force option: 
+    ```
+    rm *.json
+    ```
+3. After you have built image, you can call python script deploy.py to publish your app to the chain. You can use it in two modes:
+    a. if you have iexec cli setup on same system just run
+
+    ```
+    python3 deployment/deploy.py --image_url "dockerhubtag" --tee [--name "your-app-name"]  [--price 5]
+    ```
+    b. if you are running iexec cli on different host, than you can use remote mode
+
+    ```
+    python3 deployment/deploy.py --image_url "sdxkeeper/gva-inference-app:scone-simple-0.0.20" --tee --mrenclave "40b336410d34f107cXXXXXXX161adf435d5b8d84dd4bb2cb1936de44e8b4a2|8c24db06044d73XXXXXf84feb65c83d6|cd12bcf962f626006a1d30620f205d1f6XXXXXX15a67d1f45d3b20398f55d23" [--name "your-app-name"] [--price 5]
+    ```
---
+++ deployment/deploy.py
@@ -0,0 +1,125 @@
+import os
+import json
+import argparse
+import docker
+import subprocess
+from colorama import Fore
+from requests import exceptions
+import getpass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='automate deployment of new image into scone.')
+    parser.add_argument('--image_url', type=str, dest='image_url', required=True,
+                        help='docker image url')
+    parser.add_argument('--name', type=str, dest='name', default='test-app',
+                        help='application name')
+
+    parser.add_argument('--mode', type=str, choices=['local', 'remote'], default='local',
+                        help='Script can operate in local and remote modes: \
+                            in local mode image will be used to extract mrenclave, \
+                            in remote mode mrenclave should be provided as argument')
+    parser.add_argument('--mrenclave', type=str, dest='mrenclave',
+                        help='mrenclave of the image')
+    parser.add_argument('--price', type=int, dest='appprice', default=0,
+                        help='price for the app, default free')
+    parser.add_argument('--tee', action='store_true', default=False,
+                        help='Use tee tag in app order')
+    parser.add_argument('--password', type=str, dest='password',
+                        help='wallet password')
+    parser.add_argument('--force', action='store_true', default=False,
+                        help='overwrite existing iexec files')
+
+    args = parser.parse_args()
+    print(args)
+    if args.tee and args.mrenclave is None:
+        print("tee apps require mrenclave to be set")
+        exit()
+
+    if not args.force and (os.path.exists("iexec.json") or os.path.exists("chain.json") or os.path.exists("orders.json") or os.path.exists("deployed.json")):
+        print("iexec.json already exists, exiting to prevent overwrite!")
+        exit()
+
+    wallet_password = None
+    if args.password is None:
+        # ask for password
+        wallet_password = getpass.getpass(prompt='Wallet password:')
+    else:
+        wallet_password = args.password
+
+    dclient = docker.from_env()
+    image_sha256 = ''
+    mrenclave = None
+    if args.mode == 'remote':
+        if args.mrenclave is None:
+            print(Fore.RED + "Please provide MREnclave for remote operation mode (image is not available to extract it automatically)")
+            exit()
+        mrenclave = args.mrenclave
+        try:
+            registry_data = dclient.images.get_registry_data(args.image_url)
+            image_sha256 = registry_data.id.split(":")[1]
+        except docker.errors.APIError as error:
+            print(Fore.RED + "Could not retrieve registry information for image %s Error: %s" %(args.image_url, error))
+            exit()
+
+
+    elif args.mode == 'local':
+        image = dclient.images.get(args.image_url)
+        image_sha256 = image.id.split(":")[1]
+        result_fp = dclient.containers.run(args.image_url, entrypoint='cat', command='/conf/fingerprint.txt')
+        #print(result_fp)
+        mrenclave = result_fp.decode("utf-8")
+
+    print(Fore.RED + "Supplied image sha256 hash is " + Fore.GREEN + "%s" % image_sha256)
+    print(Fore.RED + "MREnclave: " + Fore.GREEN + "%s" % mrenclave)
+
+    completed = subprocess.run(["iexec","init", "--skip-wallet"])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
+    completed = subprocess.run(["iexec","app", "init"])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
+    iexec = json.loads(open("iexec.json").read())
+    #print(iexec)
+
+    iexec["app"]["name"] = args.name
+    iexec["app"]["multiaddr"] = "registry.hub.docker.com/" + args.image_url
+    iexec["app"]["checksum"] = "0x" + image_sha256
+
+    iexec["app"]["mrenclave"] = args.mrenclave if args.mrenclave is not None else ""
+
+    print(json.dumps(iexec["app"], sort_keys=True, indent=4))
+    json.dump(iexec, open("iexec.json", "w"))
+
+    completed = subprocess.run(["iexec","app", "deploy", "--password", wallet_password])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
+    completed = subprocess.run(["iexec","order", "init", "--app"])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
+    iexec = json.loads(open("iexec.json").read())
+
+    iexec["order"]["apporder"]["tag"] = "0x0000000000000000000000000000000000000000000000000000000000000001" if args.tee else "0x0000000000000000000000000000000000000000000000000000000000000000"
+    
+    iexec["order"]["apporder"]["appprice"] = args.appprice
+    print(json.dumps(iexec["order"]["apporder"], sort_keys=True, indent=4))
+
+    json.dump(iexec, open("iexec.json", "w"))
+
+    completed = subprocess.run(["iexec","order", "sign", "--app",  "--password", wallet_password])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
+    completed = subprocess.run(["iexec","order", "publish", "--app", "--force", "--password", wallet_password])
+    if completed.returncode != 0:
+        print("call to iexec failed")
+        exit(completed.returncode)
+
